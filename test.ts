799098d8d2b16f4148af8166db77588f7bb3a17a
p2r3 <p2r3@p2r3.com>
2026-01-03 17:26:10 +0200

    chore: move class files to a dedicated directory

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import { existsSync, readFileSync } from "fs";
-import { Demo } from "./Demo.ts";
+import { Demo } from "./sppd/Demo.ts";
 
 if (typeof process.argv[2] !== "string") {
   console.log("Usage: test.js <file.dem>");
